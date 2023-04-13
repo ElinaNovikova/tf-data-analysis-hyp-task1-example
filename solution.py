3e166bbf6019e7f07e0cae89a4558dbcd413f9b1
Элина Новикова <118816624+ElinaNovikova@users.noreply.github.com>
2023-04-13 10:28:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,25 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1112920502 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    x_conv = x_success / x_cnt
+    y_conv = y_success / y_cnt
+    
+    pooled_conv = (x_success + y_success) / (x_cnt + y_cnt)
+    se_pooled = np.sqrt(pooled_conv * (1 - pooled_conv) * (1/x_cnt + 1/y_cnt))
+    z_score = (y_conv - x_conv) / se_pooled
+    
+    alpha = 0.03
+    z_critical = norm.ppf(alpha/2)
+    
+    if z_score < -z_critical or z_score > z_critical:
+        return True # Отключение рекламы выгодно
+    else:
+        return False # Отключение рекламы не выгодно
